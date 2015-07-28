a90b7f86a81448c66131c13aeb3f5e349ab74724
gerard-geer <gerarddgeer@gmail.com>
2015-07-28 17:20:41 -0400

    The index generator now assumes png when not given a filename, and also now allows for repeat processing

--- scripts/indexgenerator.py
+++ scripts/indexgenerator.py
@@ -1,38 +1,51 @@
 from PIL import Image
 
 
-# Get the image name.
-filename = raw_input('Image name: ')
-# Load the image. 
-im = Image.open(filename)
+if __name__ == "__main__":
 
-# Get the colors used in the image.
-colors = im.getcolors()
-
-# Create a dictionary to link colors to palette indices.
-palette = {}
-
-# For each tuple returned by getcolors(), we take the
-# color, make it a string, use it as a key, then
-# at that key store the palette index.
-for t in colors:
-	palette[str(t[1])] = len(palette.keys())
+	again = True
 	
-# Print the color->index binding.
-for key, value in palette.iteritems():
-	print str(value) + ': ' + key
+	while again:
+		# Get the image name.
+		filename = raw_input('Image name: ')
+
+		# If there is no filename, we assume it's a .PNG.
+		if not filename.endswith(('.png', '.PNG', '.jpg', '.jpeg', '.dds', '.raw')):
+			filename += '.png'
+
+		# Load the image. 
+		im = Image.open(filename)
+
+		# Get the colors used in the image.
+		colors = im.getcolors()
+
+		# Create a dictionary to link colors to palette indices.
+		palette = {}
+
+		# For each tuple returned by getcolors(), we take the
+		# color, make it a string, use it as a key, then
+		# at that key store the palette index.
+		for t in colors:
+			palette[str(t[1])] = len(palette.keys())
+			
+		# Print the color->index binding.
+		for key, value in palette.iteritems():
+			print str(value) + ': ' + key
 
 
-# Get a list of pixels.
-pixels = im.getdata()
+		# Get a list of pixels.
+		pixels = im.getdata()
 
-# Create a place to store our converted data.
-indices = []
-for i in range(im.size[1]): # Outer loop loops over height.
-	indices.append([])
-	for j in range(im.size[0]): # Inner? Width.
-		indices[i].append( str(palette[ str( pixels[i*im.size[0]+j] ) ]) )
+		# Create a place to store our converted data.
+		indices = []
+		for i in range(im.size[1]): # Outer loop loops over height.
+			indices.append([])
+			for j in range(im.size[0]): # Inner? Width.
+				indices[i].append( str(palette[ str( pixels[i*im.size[0]+j] ) ]) )
 
-# Now we can print the output.
-for l in indices:
-	print(' '.join(l))
+		# Now we can print the output.
+		for l in indices:
+			print(' '.join(l))
+			
+		# Do we want to do another?
+		again = raw_input('Again? (y/n)') == 'y'
